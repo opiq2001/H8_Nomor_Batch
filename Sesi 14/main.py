b01ab6d4b818ae70ef180c48d2c8401ac543e9eb
opiq2001 <100924156+opiq2001@users.noreply.github.com>
2024-01-19 18:16:11 +0700

    add sesi 14

---
+++ Sesi 14/main.py
@@ -0,0 +1,9 @@
+
+import my_module
+
+message = my_module.greet("Alicee")
+print(message)
+
+radius = 9
+area = my_module.pi * my_module.square(radius)
+print(f"Luas Lingkaran dengan radius {radius} adalah {area:.2f}")
